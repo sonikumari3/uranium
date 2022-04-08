426af8f28a3af11c54c2fd07d1acf94a53801b20
sonishinu <sk870221@gmail.com>
2022-04-08 23:16:34 +0530

    my chnage

--- src/routes/route.js
+++ src/routes/route.js
@@ -16,9 +16,40 @@ router.get('/test-me', function (req, res) {
     console.log('These are the request query parameters: ', req.query)
     res.send('My first ever api!')
 });
+// problem 1
+//Create an API for GET /movies that returns a list of movies. Define an array of movies in your code and return the value in response.
+const arr1 = ['DDLG','PK','ZNMD','ABCD']
+router.get('/movies1',function(req,res){
+res.send(arr1)
+});
+// Create an API GET /movies/:indexNumber (For example GET /movies/1 is a valid request and it should return the movie in your array at index 1). You can define an array of movies again in your api
+router.get('/movies2/:indexNumber',function (req,res){
+let v=req.params.indexNumber
+res.send(arr1[v])
+});
+//Create an API GET /movies/:indexNumber (For example GET /movies/1 is a valid request and it should return the movie in your array at index 1). You can define an array of movies again in your api
 
 
 
+//Write another api called GET /films. Instead of an array of strings define an array of movie objects this time. Each movie object should have values - id, name. An example of movies array is 
 
+
+    [ {
+        “id”: 1,
+        “”: "DDLJ"
+       }, {
+        “id”: 2,
+        “name”: "PK"
+       }, {
+        “id”: 3,
+        “name”: "ZNMD"
+       }, {
+        “id”: 4,
+        “name”: "ABCD"
+       }]
+       router.get('/movies2/:indexNumber',function (req,res){
+        let v=req.params.indexNumber
+        res.send(arr1[v])
+       
 module.exports = router;
 // adding this comment for no reason
