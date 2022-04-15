404fc3ec7f85c5e7d0f3119729fb438283a365b7
sonishinu <sk870221@gmail.com>
2022-04-15 12:47:15 +0530

    my first mongoosedb

---
+++ src/models/authorModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const authorSchema = new mongoose.Schema(
+  {
+    author_id: {
+      type: Number,
+      required: true,
+    },
+    author_name: {
+      type: String,
+      required: true,
+    },
+    age: Number,
+    address: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("authormodel", authorSchema);
