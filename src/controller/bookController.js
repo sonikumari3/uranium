f87686e757005fb366e1d6d6db83e6fd60cfee52
sonishinu <sk870221@gmail.com>
2022-05-23 17:04:11 +0530

    final working

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -4,8 +4,42 @@ const moment = require('moment')
 const bookModel = require('../model/bookModel')
 const reviewModel = require('../model/reviewModel')
 const validator = require('../validator/validator')
-
+const aws = require('aws-sdk')
 /*******************************Create Book ************************************** */
+aws.config.update({
+    accessKeyId: "AKIAY3L35MCRUJ6WPO6J",
+    secretAccessKey: "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",
+    region: "ap-south-1"
+})
+
+let uploadFile= async ( file) =>{
+    return new Promise( function(resolve, reject) {
+     // this function will upload file to aws and return the link
+     let s3= new aws.S3({apiVersion: '2006-03-01'}); // we will be using the s3 service of aws
+ 
+     var uploadParams= {
+         ACL: "public-read",
+         Bucket: "classroom-training-bucket",  //HERE
+         Key: "abc/" + file.originalname, //HERE 
+         Body: file.buffer
+     }
+ 
+ 
+     s3.upload( uploadParams, function (err, data ){
+         if(err) {
+             return reject({"error": err})
+         }
+         console.log(data)
+         console.log("file uploaded succesfully")
+         return resolve(data.Location)
+     })
+ 
+     // let data= await s3.upload( uploadParams)
+     // if( data) return data.Location
+     // else return "there is an error"
+ 
+    })
+ }
 const createBooks = async function (req, res) {
     try{
     let data = req.body
@@ -80,8 +114,20 @@ const createBooks = async function (req, res) {
     if (!(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/).test(data.releasedAt)) {
         return res.status(400).send({ status: false, message: 'Please enter date in given format' })
     }
+    let files = req.files
+        
+        if(!files || files.length==0){ 
+           return res.status(400).send({ status : false,  msg: "No file found" })
+        }
+
+           //upload to s3 and get the uploaded link
+            // res.send the link back to frontend/postman
+
+        let uploadedFileURL= await uploadFile( files[0] )
+            data.bookCover = uploadedFileURL
 
     //  creating book document in book model
+
 
     let saveData = await bookModel.create(data)
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require('./route/route')
-
 const app = express()
+const multer = require('multer')
+const { AppConfig } = require('aws-sdk');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use( multer().any())
+
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({extended:true}))
 
@@ -13,6 +18,7 @@ mongoose.connect('mongodb+srv://jaganreddy-functionup:ORj2ygJHT7jbS3y8@cluster0.
 .then (() => console.log('mongoDB is connected '))
 .catch(err => console.log(err))
 app.use("/",router)
+
 
 app.listen(process.env.PORT || 3000,function(){
     console.log("Express app running on PORT "+(process.env.PORT || 3000))
